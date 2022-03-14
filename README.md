2a8dfeaecc570b88b9b2caa127a181753c4978a2
“Fortcake” <forrtcake@gmail.com>
2022-03-15 03:44:23 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fortcake Frontend POL Swap
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x831753DD7087CaC61aB5644b308642cc1c33Dc13',
+  busdAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+  lpAddress: '0x02E0e14D65175420fB12AcD325A7Be456bc22005'
+}
+
+export default priceContracts
